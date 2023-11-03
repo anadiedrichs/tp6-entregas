211b358b3ffb1dd2cd492a6dd8433c1cb18d5889
Lucio Román Camioli <143849254+RomanCamioli@users.noreply.github.com>
2023-11-03 20:24:20 -0300

    Update 20:23 ejercicio1.ino
    Se cambiaron las entradas de los led y pulsadores, no funciona nada jaja

--- Ejercicio 1/ejercicio1.ino
+++ Ejercicio 1/ejercicio1.ino
@@ -10,35 +10,47 @@ c) El botón PULSADOR3 cambiaría el led a ser utilizado por el LED2, para
 ejecutar a) y b) sobre LED2. Si nuevamente es presionado, cambiaría el led
 a ser utilizado por LED1, y así sucesivamente.**/
 
-int LED1 = 11 , LED2 = 9;
-int PULSADOR1 = 0 , PULSADOR2 = 2 , PULSADOR3 = 3;
-int estado1 = LOW , estado2 = LOW;
+int LED1 = 3 , LED2 = 4;
+int PULSADOR1 = 5 , PULSADOR2 = 6;
 int brillo=0;
+int estado1, estado2;
 
 void setup()
 {
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
 
-  
   pinMode(PULSADOR1, INPUT);
   pinMode(PULSADOR2, INPUT);
-  pinMode(PULSADOR3, INPUT);
-  
   Serial.begin(9600);
-};
+};//SETUP
 
 void loop()
 {
- estado1 = analogRead(PULSADOR1);
- estado2 = analogRead(PULSADOR2);
   
-  if(estado1 != LOW && brillo < 120){
+   estado1 = digitalRead(PULSADOR1);
+   estado2 = digitalRead(PULSADOR2);
+  
+  Serial.println(estado1);
+  Serial.println(estado2);
+  
+    
+  if(estado1 != HIGH  && brillo < 120){
+    
     analogWrite(LED1, brillo); 
     brillo++;
     delay(1000);
-  }
+    
+  }//IF
   
- 
+  if(estado2 == HIGH){
+    
+    analogWrite(LED1, brillo);
+    brillo--;
+    delay(1000);
+    
+  }//IF
   
-}
+  delay(1000);
+  
+}//LOOP
