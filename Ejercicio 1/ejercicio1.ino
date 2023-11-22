99b9e29514b798f15f4f5929f1f835bbb0de3475
Lucio Román Camioli <143849254+RomanCamioli@users.noreply.github.com>
2023-11-22 20:07:53 -0300

    Ejercicio1 finalizado

--- Ejercicio 1/ejercicio1.ino
+++ Ejercicio 1/ejercicio1.ino
@@ -10,10 +10,10 @@ c) El botón PULSADOR3 cambiaría el led a ser utilizado por el LED2, para
 ejecutar a) y b) sobre LED2. Si nuevamente es presionado, cambiaría el led
 a ser utilizado por LED1, y así sucesivamente.**/
 
-int LED1 = 3 , LED2 = 4;
-int PULSADOR1 = 5 , PULSADOR2 = 6;
+int LED1 = 11 , LED2 = 9;
+int PULSADOR1 = 6 , PULSADOR2 = 3, PULSADOR3 = 5;
 int brillo=0;
-int estado1, estado2;
+int estado1, estado2, estado3, condicion, condicionLED;//condicion es una bandera
 
 void setup()
 {
@@ -23,33 +23,62 @@ void setup()
   pinMode(PULSADOR1, INPUT);
   pinMode(PULSADOR2, INPUT);
   Serial.begin(9600);
+  brillo = 0;
+  condicionLED = LED1;
 };//SETUP
 
 void loop()
 {
-  
+ 	
    estado1 = digitalRead(PULSADOR1);
    estado2 = digitalRead(PULSADOR2);
+   estado3 = digitalRead(PULSADOR3);
   
-  Serial.println(estado1);
-  Serial.println(estado2);
+ Serial.print("Pulsador 1: ");
+ Serial.println(estado1);
+ Serial.print("Pulsador 2: ");
+ Serial.println(estado2);
+ Serial.print("Pulsador 3: ");
+ Serial.println(estado3);
+ Serial.print("Brillo: ");
+ Serial.println(brillo);
+ Serial.print("LED: ");
+ Serial.println(condicionLED);
   
     
-  if(estado1 != HIGH  && brillo < 120){
-    
-    analogWrite(LED1, brillo); 
-    brillo++;
-    delay(1000);
-    
-  }//IF
+  if(estado1 == HIGH && brillo <= 50){
+    condicion = 0;
+    analogWrite(condicionLED, brillo); 
+    brillo+=5;
+  
+  }//if
   
   if(estado2 == HIGH){
+    condicion = 1;
+	}//if
+
+ if(condicion == 1){
+   digitalWrite(condicionLED, HIGH);
+  
+   for(brillo = 90 ; brillo > 0 ; brillo--){
+     analogWrite(condicionLED, brillo);
+     delay(5);
+   }//for
+   
+    condicion = 0;
+}//if
+  
+ if(estado3 == HIGH && condicionLED == LED1){
+   condicionLED = LED2;
+   
+ }//if
+  
+ else if(estado3 == HIGH && condicionLED == LED2){
+    condicionLED = LED1;
     
-    analogWrite(LED1, brillo);
-    brillo--;
-    delay(1000);
-    
-  }//IF
+  }//if
+
+}//LOOP
   
   delay(1000);
   
